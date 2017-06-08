8c4d64a59e66092c38d1ecc44d73a67dc41d712e
Andrea Funtò <dihedron.dev@gmail.com>
2017-06-08 23:30:31 +0200

    Completed colorisation.

--- handler.go
+++ handler.go
@@ -5,6 +5,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"log"
+	"regexp"
 	"strings"
 
 	"github.com/dihedron/jted/stack"
@@ -20,9 +21,20 @@ type Handler struct {
 	stack  *stack.Stack
 	data   string
 	buffer bytes.Buffer
-	green  Colorf
-	yellow Colorf
-	red    Colorf
+}
+
+var (
+	pattern *regexp.Regexp
+	green   Colorf
+	yellow  Colorf
+	red     Colorf
+)
+
+func init() {
+	pattern, _ = regexp.Compile(`^{{[^}}]*}}$`)
+	green = color.New(color.FgGreen, color.Bold).SprintFunc()
+	yellow = color.New(color.FgYellow, color.Bold).SprintFunc()
+	red = color.New(color.FgRed, color.Bold).SprintFunc()
 }
 
 // Node describes a node in the XML tree.
@@ -37,15 +49,6 @@ func (h *Handler) OnStartDocument() error {
 	h.stack.Clear()
 	h.data = ""
 	h.buffer.Reset()
-	if h.green == nil {
-		h.green = color.New(color.FgGreen, color.Bold).SprintFunc()
-	}
-	if h.yellow == nil {
-		h.yellow = color.New(color.FgYellow, color.Bold).SprintFunc()
-	}
-	if h.red == nil {
-		h.red = color.New(color.FgRed, color.Bold).SprintFunc()
-	}
 	return nil
 }
 
@@ -64,7 +67,12 @@ func (h *Handler) OnStartElement(element xml.StartElement) error {
 		var buffer bytes.Buffer
 		if len(h.stack.Top().(*Node).xml.(xml.StartElement).Attr) > 0 {
 			for _, attr := range h.stack.Top().(*Node).xml.(xml.StartElement).Attr {
-				buffer.WriteString(fmt.Sprintf(" %s=\"%s\"", attr.Name.Local, h.yellow(attr.Value)))
+				if pattern.MatchString(attr.Value) {
+					buffer.WriteString(fmt.Sprintf(" %s=\"%s\"", attr.Name.Local, green(attr.Value)))
+				} else {
+					buffer.WriteString(fmt.Sprintf(" %s=\"%s\"", attr.Name.Local, yellow(attr.Value)))
+				}
+
 			}
 		}
 		log.Printf("%s<%s%s>\n", tab(h.stack.Len()-1), h.stack.Top().(*Node).xml.(xml.StartElement).Name.Local, buffer.String())
@@ -84,13 +92,17 @@ func (h *Handler) OnEndElement(element xml.EndElement) error {
 		}
 	}
 	if len(h.data) > 0 {
-		log.Printf("%s<%s%s>%s</%s>\n", tab(h.stack.Len()-1), top.Name.Local, buffer.String(), h.red(h.data), element.Name.Local)
+		if pattern.MatchString(h.data) {
+			log.Printf("%s<%s%s>%s</%s>\n", tab(h.stack.Len()-1), top.Name.Local, buffer.String(), green(h.data), element.Name.Local)
+		} else {
+			log.Printf("%s<%s%s>%s</%s>\n", tab(h.stack.Len()-1), top.Name.Local, buffer.String(), red(h.data), element.Name.Local)
+		}
 		h.data = ""
 	} else if h.stack.Top() != nil && h.stack.Top().(*Node).container {
 		log.Printf("%s</%s>\n", tab(h.stack.Len()-1), element.Name.Local)
 	} else {
 		//log.Printf("%s<%s%s/>\n", tab(h.stack.Len()-1), element.Name.Local, buffer.String())
-		log.Printf("%s<%s%s>%s</%s>\n", tab(h.stack.Len()-1), top.Name.Local, buffer.String(), h.yellow("???"), element.Name.Local)
+		log.Printf("%s<%s%s>%s</%s>\n", tab(h.stack.Len()-1), top.Name.Local, buffer.String(), yellow("???"), element.Name.Local)
 	}
 	h.stack.Pop()
 	return nil
